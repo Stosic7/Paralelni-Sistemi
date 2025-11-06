bfc0b640bda4d3abaf23d6fe2ae7df7abb96b04a
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-11-06 15:29:16 +0100

    Refactor MPI program to handle diagonal communication

--- Blanketi/Januar 2025/MPI_Zadatak_b.c
+++ Blanketi/Januar 2025/MPI_Zadatak_b.c
@@ -1,77 +1,68 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <limits.h>
-#define n 5
+
+#define n 6
+#define m 6
 
 int main(int argc, char** argv) {
-    int rank, p, i ,j;
+    int size, rank;
     int irow, jcol;
-    int glavna_dijagonala;
 
     MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-    MPI_Comm main_diag_comm;
+    int A[n][m];
+    int vrsta[m];
+
     irow = rank / n;
     jcol = rank % n;
 
-    glavna_dijagonala = (irow == jcol);
-    printf("main_diag_comm value for indexes: %d, [%d, %d]\n", glavna_dijagonala, irow, jcol);
-
-
-    int color = glavna_dijagonala ? 0 : MPI_UNDEFINED;
-    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &main_diag_comm);
-
-    if (main_diag_comm != MPI_COMM_NULL) {
-        int main_rank, main_size;
-        MPI_Comm_rank(main_diag_comm, &main_rank);
-        MPI_Comm_size(main_diag_comm, &main_size);
-
-        printf("Process %d (globalni) je %d (novi komunikator)\n", rank, main_rank);
+    int condition = (irow == jcol);
+    int color;
+    
+    if (condition) {
+        color = 1;
+    } else {
+        color = MPI_UNDEFINED;
     }
 
-    static int A[n][n];
-    int vrste[n];
+    MPI_Comm diag_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &diag_comm);
 
-    if (rank == 0) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                A[i][j] = i * n + j + 1;
-            }
+    MPI_Datatype row;
+    MPI_Type_contiguous(m, MPI_INT, &row);
+    MPI_Type_commit(&row);
+
+    if (color != MPI_UNDEFINED) {
+        int diagRank, diagSize;
+        MPI_Comm_size(diag_comm, &diagRank);
+        MPI_Comm_rank(diag_comm, &diagSize);
+
+        if (rank == 0) {
+            printf("Inicijalna matrica A:\n");
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < m; j++){
+                    A[i][j] = (i + j) % 10;
+                    printf("%d\t", A[i][j]);
+                }
+                printf("\n");
+            }     
+            fflush(stdout);
         }
-        
-        printf("Matrica A:\n");
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                printf("%4d", A[i][j]);
-            }
-            printf("\n");
+
+        MPI_Scatter(A, 1, row, vrsta, m, MPI_INT, 0, diag_comm);
+        MPI_Barrier(diag_comm);
+
+        printf("\nProces %d elementi: ", rank);
+        for (int i = 0; i < m; i++){
+            printf("%d ", vrsta[i]);
+            fflush(stdout);
         }
-        printf("-------------------\n");
     }
 
-    MPI_Bcast(&A[0][0], n*n, MPI_INT, 0, MPI_COMM_WORLD);
-
-    if (main_diag_comm != MPI_COMM_NULL) {
-        int main_rank;
-        MPI_Comm_rank(main_diag_comm, &main_rank);
-
-        MPI_Scatter(&A[0][0], n, MPI_INT, vrste, n, MPI_INT, 0, main_diag_comm);
-
-        printf("Process %d, sa identifikatorom: %d je primio vrstu: ", rank, main_rank);
-        for (i = 0; i < n; i++)
-            printf("%4d", vrste[i]);
-        printf("\n");
-
-        int suma = 0;
-        for (i = 0; i < n; i++)
-            suma += vrste[i];
-        
-        printf("Suma vrste primljene od processa sa identifikatorom %d je: %d\n", main_rank, suma);
-
-        MPI_Comm_free(&main_diag_comm);
-    }
+    fflush(stdout);
+    MPI_Type_free(&row);
 
     MPI_Finalize();
     return 0;
