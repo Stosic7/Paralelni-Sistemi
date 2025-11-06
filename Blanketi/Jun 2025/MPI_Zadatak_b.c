60c35ad0d04ac48c7659d60da8f7e84d809a9c07
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-11-06 20:32:31 +0100

    Refactor MPI program for matrix operations

--- Blanketi/Jun 2025/MPI_Zadatak_b.c
+++ Blanketi/Jun 2025/MPI_Zadatak_b.c
@@ -1,95 +1,88 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <stdlib.h>
 #include <limits.h>
 
-#define n 5
+#define n 6
+#define m 6
+#define x 5
 
 int main(int argc, char** argv) {
-    int rank, p, i, j;
-    int x = 5; // ispitujemo deljivost
+	int rank, size;
+	int key = 1;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+	int A[n][m];
+	int irow, jcol;
+	int kolona[n];
+	int primljeneSume[n];
+	int l = 0;
 
-    MPI_Datatype col_type, col_res;
-    MPI_Type_vector(n, 1, n, MPI_INT, &col_type);
-    MPI_Type_create_resized(col_type, 0, sizeof(int), &col_res);
-    MPI_Type_free(&col_type);
-    MPI_Type_commit(&col_res);
+	irow = rank / n;
+	jcol = rank % n;
 
-    int irow = rank / n;
-    int jcol = rank % n;
+	int condition = (rank % x == 0);
+	int color;
 
-    int in_new_comm = (rank % x == 0);
-    printf("in_new_comm value for indexes: %d, [%d, %d]\n", in_new_comm, irow, jcol);
+	if (condition) {
+		color = 1;
+	} else {
+		color = MPI_UNDEFINED;
+	}
 
-    MPI_Comm div_comm;
-    int color = in_new_comm ? 0 : MPI_UNDEFINED;
+	MPI_Comm div_comm;
+	MPI_Comm_split(MPI_COMM_WORLD, color, key, &div_comm);
 
-    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &div_comm);
+	MPI_Datatype col_row, col_vec;
+	MPI_Type_vector(n, 1, m, MPI_INT, &col_row);
+	MPI_Type_create_resized(col_row, 0, sizeof(int), &col_vec);
+	MPI_Type_free(&col_row);
+	MPI_Type_commit(&col_vec);
 
-    if (div_comm != MPI_COMM_NULL) {
-        int div_rank, div_size;
-        MPI_Comm_rank(div_comm, &div_rank);
-        MPI_Comm_size(div_comm, &div_size);
+	if (color != MPI_UNDEFINED) {
+		int divRank, divSize;
+		MPI_Comm_rank(div_comm, &divRank);
+		MPI_Comm_size(div_comm, &divSize);
 
-        printf("Process %d (globalni) je %d (novi komunikator)\n", rank, div_rank);
-    }
+		if (rank == 0) {
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < m; j++)
+					A[i][j] = (i + j) % 10;
+			printf("Inicijalna matrica A:\n");
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) printf("%d\t", A[i][j]);
+				printf("\n");
+			}
+			fflush(stdout);
+		}
 
-    static int A[n][n];
-    int kolona[n];
-    
-    if (rank == 0) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                A[i][j] = i * n + j + 1;
-            }
-        }
-        
-        printf("Matrica A:\n");
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                printf("%4d", A[i][j]);
-            }
-            printf("\n");
-        }
-        printf("-------------------\n");
-    }
+		MPI_Scatter(A, 1, col_vec, kolona, n, MPI_INT, 0, div_comm);
+		MPI_Barrier(div_comm);
 
-    MPI_Bcast(&A[0][0], n * n, MPI_INT, 0, MPI_COMM_WORLD);
+		printf("\n[divRank %d] kolona:\n", divRank);
+		for (int i = 0; i < n; i++) printf("%d ", kolona[i]);
+		printf("\n");
+		fflush(stdout);
 
-    if (div_comm != MPI_COMM_NULL) {
-        int div_rank;
-        MPI_Comm_rank(div_comm, &div_rank);
+		int suma = 0;
+		for (int i = 0; i < n; i++) suma += kolona[i];
+		
+		int sumsBuf[m*n];
 
-        MPI_Scatter(&A[0][0], 1, col_res, kolona, n, MPI_INT, 0, div_comm);
+		MPI_Gather(&suma, 1, MPI_INT, sumsBuf, 1, MPI_INT, 0, div_comm);
 
-        printf("Proces %d primio kolonu: ", rank);
-        for (j = 0; j < n; j++) {
-            printf("%4d", kolona[j]);
-        }
-        printf("\n");
+		if (divRank == 0) {
+			int minEl = INT_MAX;
+			for (int i = 0; i < divSize; i++)
+				if (sumsBuf[i] < minEl) minEl = sumsBuf[i];
+			printf("Minimalna suma kolone: %d\n", minEl);
+		}
+	}
 
-        int proizvod = 1;
-        for (j = 0; j < n; j++) {
-            proizvod *= kolona[j];
-        }
+	MPI_Type_free(&col_vec);
 
-        printf("Process %d: proizvod = %d\n", rank, proizvod);
+	MPI_Finalize();
+	return 0;
 
-        int max_proizvod;
-        MPI_Reduce(&proizvod, &max_proizvod, 1, MPI_INT, MPI_MAX, 0, div_comm);
-
-        if (div_rank == 0) {
-            printf("MAKSIMUM PROIZVODA: %d\n", max_proizvod);
-        }
-
-        MPI_Comm_free(&div_comm);
-        MPI_Type_free(&col_res);
-    }
-
-    MPI_Finalize();
-    return 0;
 }
