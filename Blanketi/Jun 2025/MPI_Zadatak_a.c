1ea708a730ddf496117b8b9dcc1e4bd30a6df075
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-11-06 19:43:18 +0100

    Update MPI program for matrix and vector operations

--- Blanketi/Jun 2025/MPI_Zadatak_a.c
+++ Blanketi/Jun 2025/MPI_Zadatak_a.c
@@ -1,105 +1,86 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <string.h>
-#include <stdlib.h>
-#define m 3
-#define k 4
+
+#define m 6
+#define k 6
+#define p 3
 
 int main(int argc, char** argv) {
-	int a[m][k], nizB[k];
-	int rank, i, j, p;
-	
+	int rank, size;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
-	
-	int num_rows = (m + p - 1) / p;
-	int nizA[num_rows * k];
-	ind sendbuf[p * num_rows * k];
-	
-	if (rank == 0) {
-        for (i = 0; i < m; i++)
-            for (j = 0; j < k; j++)
-                a[i][j] = i * k + j + 1;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-        printf("Matrica A:\n");
-        for (i = 0; i < m; i++) {
-            for (j = 0; j < k; j++)
-                printf("%4d", a[i][j]);
-            printf("\n");
-        }
-        printf("-------------------\n");
-
-        for (i = 0; i < k; i++)
-            b[i] = i + 1;
-
-        printf("Vektor b: ");
-        for (i = 0; i < k; i++)
-            printf("%4d", b[i]);
-        printf("\n-------------------\n");
-
-        int idx = 0;
-        for (int proc = 0; proc < p; proc++)
-            for (int row = proc; row < m; row += p)
-                for (j = 0; j < k; j++)
-                    sendbuf[idx++] = a[row][j];
-        while (idx < p * num_rows * k)
-            sendbuf[idx++] = 0;
-    }
-	
-	MPI_Scatter(sendbuf, num_rows * k, MPI_INT, nizA, num_rows * k, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(b, k, MPI_INT, 0, MPI_COMM_WORLD);
-	
-	int local_result[num_rows];
-	
-	  for (i = 0; i < num_rows; i++) {
-        int global_row = rank + i * p;
-        
-        if (global_row < m) {
-            local_result[i] = 0;
-            
-            for (j = 0; j < k; j++) {
-                local_result[i] += nizA[i * k + j] * b[j];
-            }
-            
-            printf("Proces %d: vrsta %d × b = %d\n", 
-                rank, global_row, local_result[i]);
-        }
-    }
-	
-	int final_result[m];
+	int A[m][k], B[k], C[m];
+	int nizA[m][k / p];
 
 	if (rank == 0) {
-        for (i = 0; i < num_rows; i++) {
-            int global_row = rank + i * p;
-            if (global_row < m)
-                final_result[global_row] = local_result[i];
-        }
-        
-        for (int proc = 1; proc < p; proc++) {
-            int proc_rows = (m + proc) / p;
-            int recv_buf[proc_rows];
-            
-            MPI_Recv(recv_buf, proc_rows, MPI_INT, proc, 0, 
-                    MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
-            for (i = 0; i < proc_rows; i++) {
-                int global_row = proc + i * p;
-                if (global_row < m)
-                    final_result[global_row] = recv_buf[i];
-            }
-        }
-	} else {
-        int actual_rows = 0;
-        for (i = 0; i < num_rows; i++) {
-            int global_row = rank + i * p;
-            if (global_row < m)
-                actual_rows++;
-        }
-        
-        MPI_Send(local_result, actual_rows, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    }
-	
+		printf("Inicijalna matrica A:\n");
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < k; j++) {
+				A[i][j] = (i + j) % 10;
+				printf("%d\t", A[i][j]);
+			}
+			printf("\n");
+		}
+		fflush(stdout);
+
+		printf("Inicijalna vector B:");
+		for (int i = 0; i < k; i++) {
+			B[i] = i + 1;
+			printf("%d ", B[i]);
+		}
+		printf("\n");
+	}
+
+	MPI_Datatype row_vec, row_type;
+	MPI_Type_vector(k / p * m, 1, p, MPI_INT, &row_vec);
+	MPI_Type_create_resized(row_vec, 0, sizeof(int), &row_type);
+	MPI_Type_free(&row_vec);
+	MPI_Type_commit(&row_type);
+
+	MPI_Scatter(A, 1, row_type, nizA, m * k / p, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	printf("\nProces %d local columns:\n", rank);
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < k / p; j++) {
+			printf("%d\t", nizA[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n-----------------------------");
+
+	MPI_Bcast(B, k, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	printf("\nProces %d local vector: ", rank);
+	for (int i = 0; i < k; i++)
+		printf("%d ", B[i]);
+	printf("\n");
+
+	int parcijalniRezultat[m];
+
+	for (int i = 0; i < m; i++) {
+		int s = 0;
+		for (int j = 0; j < k/p; j++) {
+			int jglob = j * p + rank;
+			s += nizA[i][j] * B[jglob];
+		}
+		parcijalniRezultat[i] = s;
+	}
+
+	MPI_Reduce(parcijalniRezultat, C, m, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+	if (rank == 0) {
+		printf("\nC: ");
+		for (int i = 0; i < m; i++)
+			printf("%d ", C[i]);
+	}
+	fflush(stdout);
+
 	MPI_Finalize();
-    return 0;
+	return 0;
 }
