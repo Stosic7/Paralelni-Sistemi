c9fb67fd011388ddb8851d1bf5620b3d3b9561b3
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-11-07 02:35:06 +0100

    Refactor MPI matrix multiplication code

--- Blanketi/Septembar 2025/MPI_Zadatak.c
+++ Blanketi/Septembar 2025/MPI_Zadatak.c
@@ -1,102 +1,102 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <limits.h>
+
 #define n 3
-#define m 2
-#define k 4
+#define m 4
+#define k 2
+#define p 2
 
 int main(int argc, char** argv) {
-    static int a[n][k];
-    static int b[k][m];
-    static int c[n][m];
-    int minElement[n]; // min elementi svake vrste
-    int rank, i, j, p;
-    int nizA[n*(k/p)], nizB[(k/p)*m];
+	int rank, size;
+	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+	int A[n][k], B[k][m], C[n][m];
+	int nizA[n][k / p];
+	int nizB[k / p][m];
 
-    MPI_Datatype col_vector, col_type;
-    MPI_Type_vector(n, k/p, k, MPI_INT, &col_vector);
-    MPI_Type_create_resized(col_vector, 0, (k/p)*sizeof(int), &col_type);
-    MPI_Type_commit(&col_type);
-    MPI_Type_free(&col_vector);
+	if (rank == 0) {
+		printf("Inicijalna matrica A:\n");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < k; j++) {
+				A[i][j] = (i*j+1+(i*i) + j * j * (i + 1*j)) % 10;
+				printf("%d\t", A[i][j]);
+			}
+			printf("\n");
+		}
+		fflush(stdout);
 
-    if (rank == 0) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < k; j++) {
-                a[i][j] = i * 2 + j;
-            }
-        }
+		printf("Inicijalna matrica B:\n");
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < m; j++) {
+				B[i][j] = (i*j+1 + j*j*(i+1)) % 20;
+				printf("%d\t", B[i][j]);
+			}
+			printf("\n");
+		}
+		fflush(stdout);
+	}
 
-        printf("MATRICA A\n\n");
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < k; j++) {
-                printf("%4d", a[i][j]);
-            }
-            printf("\n");
-        }
+	MPI_Datatype col_vec, col_type;
+	MPI_Type_vector(n, k / p, k, MPI_INT, &col_vec);
+	MPI_Type_create_resized(col_vec, 0, (k / p) * sizeof(int), &col_type);
+	MPI_Type_free(&col_vec);
+	MPI_Type_commit(&col_type);
 
-        for (i = 0; i < n; i++) {
-            int min = INT_MAX;
-            for (j = 0; j < k; j++) {
-                if (a[i][j] < min) {
-                    min = a[i][j];
-                }
-            }
-            minElement[i] = min;
-        }
+	MPI_Scatter(A, 1, col_type, nizA, n * (k / p), MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
 
-        printf("\nminimalni elementi matrice A: \n");
-        for (i = 0; i < n; i++)
-            printf("%2d", minElement[i]);
-        
-        printf("\n\n");
-        
-        for (i = 0; i < k; i++) {
-            for (j = 0; j < m; j++) {
-                b[i][j] = i + j + 1;
-            }
-        }
+	printf("\nProces %d (matrix A):\n", rank);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < k / p; j++) {
+			printf("%d\t", nizA[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n-----------------------------");
 
-        printf("MATRICA B\n\n");
-        for (i = 0; i < k; i++) {
-            for (j = 0; j < m; j++) {
-                printf("%2d", b[i][j]);
-            }
-            printf("\n");
-        }
+	MPI_Datatype row_vec;
+	MPI_Type_vector(k / p, m, m, MPI_INT, &row_vec);
+	MPI_Type_commit(&row_vec);
 
-        printf("----------\n");
-    }
+	MPI_Scatter(B, 1, row_vec, nizB, (k / p) * m, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
 
-    MPI_Scatter(&a[0][0], 1, col_type, nizA, n*(k/p), MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter(&b[0][0], (k/p)*m, MPI_INT, nizB, (k/p)*m, MPI_INT, 0, MPI_COMM_WORLD);
+	printf("\nProces %d (matrix B):\n", rank);
+	for (int i = 0; i < k/p; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%d\t", nizB[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n-----------------------------");
 
-    int parcijalno_c[n][m];
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            parcijalno_c[i][j] = 0;
-            for (int t = 0; t < k/p; t++) {
-                parcijalno_c[i][j] += nizA[i*(k/p) + t] * nizB[t*m + j];
-            }
-        }
-    }
+	int parcijalno_c[n][m];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			parcijalno_c[i][j] = 0;
+			for (int t = 0; t < k / p; t++) {
+				parcijalno_c[i][j] += nizA[i][t] * nizB[t][j];
+			}
+		}
+	}
 
-    MPI_Reduce(parcijalno_c, c, n*m, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(parcijalno_c, C, n * m, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
 
-    if (rank == 0) {
-        printf("\nRezultujuca Matrica C (A x B):\n\n");
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) {
-                printf("%4d", c[i][j]);
-            }
-            printf("\n");
-        }
-    }
+	printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+	if (rank == 0) {
+		printf("\nC: \n");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				printf("%d\t", C[i][j]);
+			}
+			printf("\n");
+		}
+	}
 
-    MPI_Type_free(&col_type);
-    MPI_Finalize();
-    return 0;
+	MPI_Finalize();
+	return 0;
 }
