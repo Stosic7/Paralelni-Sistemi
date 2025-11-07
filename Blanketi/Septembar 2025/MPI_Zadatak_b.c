ef6c7bd1eeda12bcb5557bb236d0a7d832e91008
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-11-07 03:03:38 +0100

    Refactor MPI program to handle matrix operations

--- Blanketi/Septembar 2025/MPI_Zadatak_b.c
+++ Blanketi/Septembar 2025/MPI_Zadatak_b.c
@@ -1,94 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
 #include <limits.h>
 
-#define n 5
+#define n 6
+#define m 6
+#define x 10
+#define p 36
 
 int main(int argc, char** argv) {
-    int rank, p, i, j;
-    int x = 10;  // Zadato x
+	int rank, size, irow, jcol, color, key = 1;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+	int A[n][m];
 
-    int irow = rank / n;  // Red
-    int jcol = rank % n;  // Kolona
+	irow = rank / n;
+	jcol = rank % n;
+	int vrsta[m];
+	int prVrsta[m];
 
-    int on_diagonal = (irow + jcol == n - 1); // sporedna dijagonala
+	int condition = (irow + jcol == n - 1);
 
-    int in_new_comm = (on_diagonal && rank < x);
-    printf("in_new_comm value for indexes: %d, [%d, %d]\n", in_new_comm, irow, jcol);
+	if (condition) {
+		color = 0;
+	} else {
+		color = MPI_UNDEFINED;
+	}
 
-    MPI_Comm diag_comm;
-    int color = in_new_comm ? 0 : MPI_UNDEFINED;
-    
-    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &diag_comm);
-    
-    if (diag_comm != MPI_COMM_NULL) {
-        int diag_rank, diag_size;
-        MPI_Comm_rank(diag_comm, &diag_rank);
-        MPI_Comm_size(diag_comm, &diag_size);
-        
-        printf("Proces %d (globalni) je %d (novi komunikator)\n", 
-               rank, diag_rank);
-    }
+	MPI_Comm diag_comm;
+	MPI_Comm_split(MPI_COMM_WORLD, color, key, &diag_comm);
 
-    static int A[n][n];
-    int vrsta[n];
-    
-    if (rank == 0) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                A[i][j] = i * n + j + 1;
-            }
-        }
-        
-        printf("Matrica A:\n");
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                printf("%4d", A[i][j]);
-            }
-            printf("\n");
-        }
-        printf("-------------------\n");
-    }
+	MPI_Datatype row;
+	MPI_Type_contiguous(m, MPI_INT, &row);
+	MPI_Type_commit(&row);
 
-    MPI_Bcast(&A[0][0], n * n, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    if (diag_comm != MPI_COMM_NULL) {
-        int diag_rank;
-        MPI_Comm_rank(diag_comm, &diag_rank);
-        
-        // Scatter redove
-        MPI_Scatter(&A[0][0], n, MPI_INT, vrsta, n, MPI_INT, 0, diag_comm);
-        
-        printf("Proces %d primio vrstu: ", rank);
-        for (j = 0; j < n; j++) {
-            printf("%4d", vrsta[j]);
-        }
-        printf("\n");
+	if (color != MPI_UNDEFINED) {
+		int diagRank, diagSize;
+		MPI_Comm_size(diag_comm, &diagSize);
+		MPI_Comm_rank(diag_comm, &diagRank);
 
-        // Proizvod elemenata vrste
-        int proizvod = 1;
-        for (j = 0; j < n; j++) {
-            proizvod *= vrsta[j];
-        }
-        
-        printf("Proces %d: proizvod = %d\n", rank, proizvod);
+		if (diagRank == 0) {
+			printf("Inicijalna matrica A:\n");
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					A[i][j] = (i + j) % 15 + 1;
+					printf("%d\t", A[i][j]);
+				}
+				printf("\n");
+			}
+			fflush(stdout);
+		}
 
-        // MPI_Reduce za maksimum proizvoda
-        int max_proizvod;
-        MPI_Reduce(&proizvod, &max_proizvod, 1, MPI_INT, MPI_MAX, 0, diag_comm);
-        
-        if (diag_rank == 0) {
-            printf("MAKSIMUM PROIZVODA: %d\n", max_proizvod);
-        }
-        
-        MPI_Comm_free(&diag_comm);
-    }
-    
-    MPI_Finalize();
-    return 0;
+		MPI_Scatter(A, 1, row, vrsta, n, MPI_INT, 0, diag_comm);
+		MPI_Barrier(diag_comm);
+
+		int proizvod = 1;
+		for (int i = 0; i < m; i++) {
+			proizvod *= vrsta[i];
+		}
+
+		MPI_Gather(&proizvod, 1, MPI_INT, prVrsta, 1, MPI_INT, 0, diag_comm);
+
+		if (diagRank == 0) {
+			printf("PROIZVODI IZ PROCESSA: \n");
+			for (int i = 0; i < m; i++) {
+				printf("%d, ", prVrsta[i]);
+			}
+			printf("\n");
+
+			int maxEl = INT_MIN;
+			for (int i = 0; i < m; i++) {
+				if (prVrsta[i] > maxEl) {
+					maxEl = prVrsta[i];
+				}
+			}
+			printf("Maksimalni proizvod: %d\n", maxEl);
+		}
+	}
+
+	MPI_Finalize();
+	return 0;
 }
