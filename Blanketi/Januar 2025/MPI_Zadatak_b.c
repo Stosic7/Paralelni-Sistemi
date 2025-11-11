6b3ee4e97a4c806579afd35925267ec2c58a80b8
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-11-11 18:18:26 +0100

    Refactor MPI program for diagonal communication

--- Blanketi/Januar 2025/MPI_Zadatak_b.c
+++ Blanketi/Januar 2025/MPI_Zadatak_b.c
@@ -1,69 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
-#define n 6
-#define m 6
+#define n 5
+#define p 25
 
 int main(int argc, char** argv) {
-    int size, rank;
-    int irow, jcol;
+	int rank, size, irow, jcol, color, key = 1;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int A[n][m];
-    int vrsta[m];
+	irow = rank / n;
+	jcol = rank % n;
 
-    irow = rank / n;
-    jcol = rank % n;
+	int condition = (irow == jcol);
+	color = condition ? 0 : MPI_UNDEFINED;
 
-    int condition = (irow == jcol);
-    int color;
-    
-    if (condition) {
-        color = 1;
-    } else {
-        color = MPI_UNDEFINED;
-    }
+	MPI_Comm diag;
+	MPI_Comm_split(MPI_COMM_WORLD, color, key, &diag);
 
-    MPI_Comm diag_comm;
-    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &diag_comm);
+	int A[n][n];
+	int rows[n];
 
-    MPI_Datatype row;
-    MPI_Type_contiguous(m, MPI_INT, &row);
-    MPI_Type_commit(&row);
+	if (color != MPI_UNDEFINED) {
+		int diagRank, diagSize;
+		MPI_Comm_rank(diag, &diagRank);
+		MPI_Comm_size(diag, &diagSize);
 
-    if (color != MPI_UNDEFINED) {
-        int diagRank, diagSize;
-        MPI_Comm_size(diag_comm, &diagSize);
-        MPI_Comm_rank(diag_comm, &diagRank);
+		if (diagRank == 0) {
+			printf("Inicijlizovana matrica A: \n");
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					A[i][j] = ((i * i + j * j) % 20) * (j * 5 + i % 2);
+					printf("%d\t", A[i][j]);
+				}
+				printf("\n");
+			}
+			printf("----------------------------------------\n");
+		}
 
-        if (diagRank == 0) {
-            printf("Inicijalna matrica A:\n");
-            for (int i = 0; i < n; i++){
-                for (int j = 0; j < m; j++){
-                    A[i][j] = (i + j) % 10;
-                    printf("%d\t", A[i][j]);
-                }
-                printf("\n");
-            }     
-            fflush(stdout);
-        }
+		MPI_Scatter(A, n, MPI_INT, rows, n, MPI_INT, 0, diag);
+		MPI_Barrier(diag);
 
-        MPI_Scatter(A, 1, row, vrsta, m, MPI_INT, 0, diag_comm);
-        MPI_Barrier(diag_comm);
+		printf("Process ID: %d u novom komunikatoru ima vrstu: ", diagRank);
+		for (int i = 0; i < n; i++) {
+			printf("%2d, ", rows[i]);
+		}
+		printf("\n----------------------------------------------------------------------------------------------\n");
 
-        printf("\nProces %d elementi: ", rank);
-        for (int i = 0; i < m; i++){
-            printf("%d ", vrsta[i]);
-            fflush(stdout);
-        }
-    }
+		int suma = 0;
+		for (int i = 0; i < n; i++) {
+			suma += rows[i];
+		}
+		
+		int* sume = NULL;
+		if (diagRank == 0) {
+			sume = (int*)malloc(diagSize * sizeof(int));
+		}
+		MPI_Gather(&suma, 1, MPI_INT, sume, 1, MPI_INT, 0, diag);
 
-    fflush(stdout);
-    MPI_Type_free(&row);
+		if (diagRank == 0) {
+			printf("\n=== FINALNI REZULTATI ===\n");
+			int ukupno = 0;
+			for (int i = 0; i < diagSize; i++) {
+				printf("Dijagonalni proces %d: suma = %d\n", i, sume[i]);
+				ukupno += sume[i];
+			}
+			printf("Ukupan zbir dijagonale: %d\n", ukupno);
+			free(sume);
+		}
+	}
 
-    MPI_Finalize();
-    return 0;
+	MPI_Finalize();
+	return 0;
 }
