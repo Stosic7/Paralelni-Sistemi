c6b6e86a225dceb2b5ce8ff701b1bf52c8dce2ff
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-11-12 20:23:40 +0100

    Refactor MPI matrix multiplication parameters and logic

--- Blanketi/Jun 2025/MPI_Zadatak_a.c
+++ Blanketi/Jun 2025/MPI_Zadatak_a.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
-#define m 6
-#define k 6
-#define p 3
+#define m 4
+#define k 5
+#define p 2
+#define W MPI_COMM_WORLD
 
 int main(int argc, char** argv) {
 	int rank, size;
 	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(W, &rank);
+	MPI_Comm_size(W, &size);
 
 	int A[m][k], B[k], C[m];
-	int nizA[m][k / p];
+	int nizA[m/p][k];
 
 	if (rank == 0) {
 		printf("Inicijalna matrica A:\n");
@@ -24,6 +26,7 @@ int main(int argc, char** argv) {
 			printf("\n");
 		}
 		fflush(stdout);
+		printf("------------------------------------------------\n");
 
 		printf("Inicijalna vector B:");
 		for (int i = 0; i < k; i++) {
@@ -31,55 +34,45 @@ int main(int argc, char** argv) {
 			printf("%d ", B[i]);
 		}
 		printf("\n");
+		printf("------------------------------------------------\n");
 	}
 
 	MPI_Datatype row_vec, row_type;
-	MPI_Type_vector(k / p * m, 1, p, MPI_INT, &row_vec);
-	MPI_Type_create_resized(row_vec, 0, sizeof(int), &row_type);
+	MPI_Type_vector(m / p, k, p * k, MPI_INT, &row_vec);
+	MPI_Type_create_resized(row_vec, 0, k*sizeof(int), &row_type);
 	MPI_Type_free(&row_vec);
 	MPI_Type_commit(&row_type);
 
-	MPI_Scatter(A, 1, row_type, nizA, m * k / p, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Scatter(A, 1, row_type, nizA, (m / p) * k, MPI_INT, 0, W);
 
-	printf("\nProces %d local columns:\n", rank);
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < k / p; j++) {
+	printf("Process %d (matrica A): \n", rank);
+	for (int i = 0; i < m/p; i++) {
+		for (int j = 0; j < k; j++) {
 			printf("%d\t", nizA[i][j]);
 		}
 		printf("\n");
 	}
-	printf("\n-----------------------------");
+	printf("------------------------------------------------\n");
 
-	MPI_Bcast(B, k, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Bcast(B, k, MPI_INT, 0, W);
 
-	printf("\nProces %d local vector: ", rank);
-	for (int i = 0; i < k; i++)
-		printf("%d ", B[i]);
-	printf("\n");
-
-	int parcijalniRezultat[m];
-
-	for (int i = 0; i < m; i++) {
-		int s = 0;
-		for (int j = 0; j < k/p; j++) {
-			int jglob = j * p + rank;
-			s += nizA[i][j] * B[jglob];
+	int parcijalni_C[m / p];
+	for (int i = 0; i < m / p; i++) {
+		parcijalni_C[i] = 0;
+		for (int j = 0; j < k; j++) {
+			parcijalni_C[i] += nizA[i][j] * B[j];
 		}
-		parcijalniRezultat[i] = s;
 	}
 
-	MPI_Reduce(parcijalniRezultat, C, m, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Gather(parcijalni_C, m / p, MPI_INT, C, m / p, MPI_INT, 0, W);
 
-	printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 	if (rank == 0) {
-		printf("\nC: ");
-		for (int i = 0; i < m; i++)
+		printf("\nVektor C = A * B:\n");
+		for (int i = 0; i < m; i++) {
 			printf("%d ", C[i]);
+		}
+		printf("\n");
 	}
-	fflush(stdout);
 
 	MPI_Finalize();
 	return 0;
